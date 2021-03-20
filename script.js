02a1a09f42910435f4dc95238cbc4146e9120363
Michayla Constantinou <michayla.constantinou@gmail.com>
2021-03-20 14:38:58 +1100

    final update

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ const symbolsElement = document.getElementById("symbols")
 
 const form = document.getElementById("passwordGenerator")
 
+const passwordDisplay = document.getElementById("passwordDisplay")
+
+// const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const UPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+
 characterAmountNumber.addEventListener("input", syncCharacterAmount);
 characterAmountRange.addEventListener("input", syncCharacterAmount);
 
@@ -19,10 +33,21 @@ form.addEventListener("submit", e => {
   const symbols = symbolsElement.checked
 
   const password = generatePassword(characterAmount, upercase, numbers, symbols)
+  passwordDisplay.innerText = password
 })
 
 function generatePassword(characterAmount, upercase, numbers, symbols) {
-  String.fromCharCode(65)
+  let charCodes = UPERCASE_CHAR_CODE
+  // if (upercase) charCodes = charCodes.concat(UPERCASE_CHAR_CODES)
+  if (numbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
+  if (symbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES)
+
+    const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random()* charCodes.length)]  
+    passwordCharacters.push(string.fromCharacteoCode(charCodes))
+  }
+  return passwordCharacters.join('')
 }
 
 function arrayFromLowToHigh(low,high) {
