879c1d4ddafa4221e0a858674e02e4e30d23411f
Michayla Constantinou <michayla.constantinou@gmail.com>
2021-05-03 20:35:51 +1000

    Readme file updated and completed

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Password Generator
 
-## 
+## The Task
+
+Modify starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that is write. It will have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+
+## User Story
+
+AS AN employee with access to sensitive data
+YOU WANT to randomly generate a password that meets certain criteria
+SO THAT YOU can create a strong password that provides greater security
--- script.js
+++ script.js
@@ -10,6 +10,13 @@ const form = document.getElementById("passwordGenerator")
 
 const passwordDisplay = document.getElementById("passwordDisplay")
 
+const resultDOM = document.getElementById('result');
+const copybtnDOM = document.getElementById('copy');
+const lengthDOM = document.getElementById('length');
+const uppercaseDOM = document.getElementById('uppercase');
+const numbersDOM = document.getElementById('numbers');
+const symbolsDOM = document.getElementById('symbols');
+const generatebtn = document.getElementById('generate');
 
 characterAmountNumber.addEventListener("input", syncCharacterAmount);
 characterAmountRange.addEventListener("input", syncCharacterAmount);
@@ -21,6 +28,14 @@ form.addEventListener("submit", e => {
   const numbers = numbersElement.checked
   const symbols = symbolsElement.checked
 
+  let generatePassword = (
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+    ) => {
+    }
+
   const password = generatePassword(characterAmount, upercase, numbers, symbols)
   passwordDisplay.innerText = password
 })
@@ -31,16 +46,39 @@ form.addEventListener("submit", e => {
     passwordCharacters.push(string.fromCharacteoCode(charCodes))
   }
 
-function arrayFromLowToHigh(low,high) {
-  const array = []
+let arrayFromLowToHigh = (low, high) => {
+  const array = [];
   for (let i = low; i <= high; i++) {
-    array.push(i)
+    array.push(i);
   }
-  return array
-}
+  return array;
+};
+
+const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
 
 function syncCharacterAmount(e) {
   const value = e.target.value
   characterAmountNumber.value = value
   characterAmountRange.value = value
-}
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const characterAmount = lengthDOM.value;
+  const includeUppercase = uppercaseDOM.checked;
+  const includeNumbers = numbersDOM.checked;
+  const includeSymbols = symbolsDOM.checked;
+  const password = generatePassword(
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+  );
+  resultDOM.innerText = password;
+});
