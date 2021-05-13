829d362c503f96b6ecc1449d8f7703a527321f80
Michayla Constantinou <michayla.constantinou@gmail.com>
2021-05-13 23:32:07 +1000

    modifing and changing

--- script.js
+++ script.js
@@ -1,4 +1,78 @@
+const amountRange = document.getElementById('amountRange')
+const amountNumber = document.getElementById('amountNumber')
+
+amountNumber.addEventListener('imput, syncAmount')
+amountRange.addEventListener('imput, syncAmount')
+
+
 
 
 var dataElement = getElementById ('owasp');
 var owasp = JSON.parse(dataElement.textContent);
+
+var generateBtn = document.querySelector("#generate");
+
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
+
+function generatePassword () {
+    var passLength = prompt("Please enter how many characters you would like your password to be. \nMust be between 8 - 128 characters.");
+    
+    var lengthConfirm = parseInt(passLength);
+    console.log(lengthConfirm);
+ 
+    if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+        alert ("Error. Choice is not valid. Please try again.");
+        return;
+    }
+
+    var collectionOfValids = [];
+var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
+var lowerCaseChar = "abcdefghiklmnopqrsttuvwxtz" .split("");
+var numberChar = "0123456789" .split ("");
+var specialChar = "*&^%$#@!?><{}" .split("");
+
+var lowerCaseConfirm = confirm("Do you want your password to include lowercase letters?");
+    if(lowerCaseConfirm === true) {
+        for (var i = 0; i < lowerCaseChar.length; i++) {
+            collectionOfValids.push(lowerCaseChar[i]);
+        }
+    }
+
+  var upperCaseConfirm = confirm("Do you want your password to include uppercase letters?");
+    if (upperCaseConfirm === true) {
+        for (var i = 0; i < upperCaseChar.length; i++) {
+            collectionOfValids.push(upperCaseChar[i]);
+        }
+    }
+
+  var numberConfirm = confirm("Do you want your password to include numbers");
+    if (numberConfirm === true) {
+        for (var i = 0; i < numberChar.length; i++) {
+            collectionOfValids.push(numberChar[i]);
+        }
+    }
+    
+var specialConfirm = confirm("Do you want your password to include special characters?");
+    if (specialConfirm === true) {
+        for (var i = 0; i < specialChar.length; i++) {
+            collectionOfValids.push(specialChar[i]);
+        }
+    }
+
+    var randomPassword = "";
+ for (var i = 0; i < lengthConfirm; i++) {
+     collectionOfValids[
+         Math.floor(Math.random() * collectionOfValids.length)];
+     randomPassword +=
+        collectionOfValids[
+            Math.floor(Math.random() * collectionOfValids.length)
+        ];
+    }
+ return randomPassword;
+}
+
+generateBtn.addEventListener("click", writePassword);
