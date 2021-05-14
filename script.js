929bb183ff4b029d18b4bebe889c68d970925d16
Michayla Constantinou <michayla.constantinou@gmail.com>
2021-05-14 22:27:51 +1000

    attempting new method

--- script.js
+++ script.js
@@ -1,78 +1,54 @@
 const amountRange = document.getElementById('amountRange')
 const amountNumber = document.getElementById('amountNumber')
+const uppercaseElement = document.getElementById('uppercase')
+const numbersElement = document.getElementById('numbers')
+const symbolsElement = document.getElementById('symbols')
+
+const form = document.getElementById('generatorForm')
+
+const lowercaseCharCodes = arryFromLowToHigh(65, 90)
+const uppercaseCharCodes = arryFromLowToHigh(97, 122)
+const numberCharCodes = arrayFromLowToHigh(48, 57)
+const symbolsCharCodes = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
 amountNumber.addEventListener('imput, syncAmount')
 amountRange.addEventListener('imput, syncAmount')
 
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    const characterAmount = amountNumber.value
+    const uppercase = uppercaseElement.checked
+    const numbers = numbersElement.checked
+    const symbols = symbolsElement.checked
+    const password = generatePassword(characterAmount, uppercase, numbers, symbols)
+})
 
+function generatePassword(characterAmount, uppercase, numbers, symbols) {
+    String.fromCharCode(65)
+}
+
+function arryFromLowToHigh(low, high) {
+    const arry = []
+    for (let i = low; i <= high; i++) {
+        arry.push(i)
+    }  
+    return arry
+}
+
+function syncAmount(e) {
+    const value = e.target.value
+    amountNumber.value = value
+    amountRange.value = value
+}
 
 
 var dataElement = getElementById ('owasp');
 var owasp = JSON.parse(dataElement.textContent);
-
-var generateBtn = document.querySelector("#generate");
-
-function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-}
-
-function generatePassword () {
-    var passLength = prompt("Please enter how many characters you would like your password to be. \nMust be between 8 - 128 characters.");
-    
-    var lengthConfirm = parseInt(passLength);
-    console.log(lengthConfirm);
- 
-    if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-        alert ("Error. Choice is not valid. Please try again.");
-        return;
-    }
-
-    var collectionOfValids = [];
-var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" .split("");
-var lowerCaseChar = "abcdefghiklmnopqrsttuvwxtz" .split("");
-var numberChar = "0123456789" .split ("");
-var specialChar = "*&^%$#@!?><{}" .split("");
-
-var lowerCaseConfirm = confirm("Do you want your password to include lowercase letters?");
-    if(lowerCaseConfirm === true) {
-        for (var i = 0; i < lowerCaseChar.length; i++) {
-            collectionOfValids.push(lowerCaseChar[i]);
-        }
-    }
-
-  var upperCaseConfirm = confirm("Do you want your password to include uppercase letters?");
-    if (upperCaseConfirm === true) {
-        for (var i = 0; i < upperCaseChar.length; i++) {
-            collectionOfValids.push(upperCaseChar[i]);
-        }
-    }
-
-  var numberConfirm = confirm("Do you want your password to include numbers");
-    if (numberConfirm === true) {
-        for (var i = 0; i < numberChar.length; i++) {
-            collectionOfValids.push(numberChar[i]);
-        }
-    }
-    
-var specialConfirm = confirm("Do you want your password to include special characters?");
-    if (specialConfirm === true) {
-        for (var i = 0; i < specialChar.length; i++) {
-            collectionOfValids.push(specialChar[i]);
-        }
-    }
-
-    var randomPassword = "";
- for (var i = 0; i < lengthConfirm; i++) {
-     collectionOfValids[
-         Math.floor(Math.random() * collectionOfValids.length)];
-     randomPassword +=
-        collectionOfValids[
-            Math.floor(Math.random() * collectionOfValids.length)
-        ];
-    }
- return randomPassword;
-}
 
 generateBtn.addEventListener("click", writePassword);
