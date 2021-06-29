cd08f98798a0ad0429dc03334eeb57f0a7bb573d
Michayla <michayla@Michaylas-MacBook-Pro.local>
2021-06-29 11:40:53 +1000

    upper + numbers

--- script.js
+++ script.js
@@ -118,6 +118,17 @@ var generatePassword = function() {
       console.log(retVal);
     }
 
+    //Only Numbers & Uppercase Chosen
+    else if (!special && numbers && !lower && upper) {
+      
+      totalLength = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      
+      for (var i = 0, n = totalLength.length; i < passwordLength; ++i) {
+        retVal += totalLength.charAt(Math.floor(Math.random() * n));
+      }
+      console.log(retVal);
+    }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
